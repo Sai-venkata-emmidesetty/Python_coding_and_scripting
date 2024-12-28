64f356cd643e66355baf3410d624843469b12c60
Venkata Sai Emmidesetty <141021513+Sai-venkata-emmidesetty@users.noreply.github.com>
2024-12-28 17:31:45 +0530

    Update anagram_hash.py

--- anagram_hash.py
+++ anagram_hash.py
@@ -13,7 +13,30 @@ strs=["eat","tea","tan","ate","nat","bat"]
 #output = [["bat"],["nat","tan"],["ate","eat","tea"]]
 print(anagrams(strs))
 
+"""
+sorted_strs=tuple(sorted(s))
+-----------------------------
+Purpose: Tuples are immutable (unchangeable) and hashable, which means they can be used as keys in dictionaries.
+How it's used here:
+sorted_strs = tuple(sorted(s))
+The code sorts the string s to create a standardized representation of all anagrams. For example, both "eat" and "tea" will become ('a', 'e', 't') after sorting.
+The tuple ('a', 'e', 't') is used as a key in the dictionary (defined_hashmap) because it represents the "signature" of an anagram group.
+"""
+"""
+Why defaultdict?
+Purpose: defaultdict simplifies handling cases where keys might not yet exist in the dictionary.
+How it's used here:
 
+defined_hashmap = defaultdict(list)
+If a key does not exist in the dictionary, defaultdict will automatically create a default value (in this case, an empty list) for that key.
+This eliminates the need to check if a key exists and initialize it manually, which would look like:
+python code example:
+if key not in hashmap:
+    hashmap[key] = []
+Simplifies Code: You can directly append to the list of anagrams for the key without worrying if the key exists,given below:
+
+defined_hashmap[sorted_strs].append(s)
+"""
 #Keys are always imutable by default
 """
 for s in strs:
